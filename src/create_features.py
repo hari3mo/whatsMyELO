4d61fad3ebb85cfef7eff5d29f2a398c50c579ea
hari3mp <harisasaif@gmail.com>
2026-05-12 17:23:07 -0700

    create features

--- src/create_features.py
+++ src/create_features.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
 
+GAMES_CSV_PATH = 'data/lichess_games.csv' # output from extract_games.py
+
 def create_player_features(row):
-    return
+    evals = np.array([float(x) for x in row['evals'].split(';') if x])
+    clocks = np.array([float(x) for x in row['clocks'].split(';') if x])
+    
+    return pd.Series([evals, clocks])
 
 def format_df():
     return
 
 def main():
-    return
+    games = pd.read_csv(GAMES_CSV_PATH)
+    features = games.apply(create_player_features, axis=1)
+    features.columns = ['evals', 'clocks']
+    return features
 
 if __name__ == "__main__":
-    main()
+    features = main()
+    
